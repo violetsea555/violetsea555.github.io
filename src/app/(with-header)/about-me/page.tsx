854b5ce7d11b293b547c853429d8fdd505adc8cf
Violet Liu <violetsea555@gmail.com>
2026-08-13 04:00:36 -0700

    about me update

--- src/app/(with-header)/about-me/page.tsx
+++ src/app/(with-header)/about-me/page.tsx
@@ -13,12 +13,13 @@ export default function AboutMe() {
                 Hi! I&apos;m Violet — a software engineer who discovered her
                 love of coding in middle school with Codecademy, and hasn&apos;t
                 stopped creating since. I have six+ years of professional
-                experience building frontend software in React & TypeScript, an
-                academic background in backend development(Python, C++) through
-                a B.S. in Computer Science from UCI, and roots in accessibility
-                research at UC Irvine. If you&apos;re interested in working with
-                me or believe I&apos;m a good fit for your company, please reach
-                out on{" "}
+                experience building software, specializing in Frontend
+                development(React & TypeScript) for the past few years. I have
+                an academic background in backend development(Python, C++)
+                through a B.S. in Computer Science from UCI, and roots in
+                accessibility research at UC Irvine. If you&apos;re interested
+                in working with me or believe I&apos;m a good fit for your
+                company, please reach out on{" "}
                 <a
                   className="text-[#f59c86]"
                   href="https://www.linkedin.com/in/avioletliu"
